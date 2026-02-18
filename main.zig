29c850784bc610594287e356bf9a5d88af18df2d
Mengkorng LY <mengkorng.ly@axivit.com>
2026-02-18 14:59:47 +0700

    main: That's awesome! let's continue

--- main.zig
+++ main.zig
@@ -1,1 +1,6 @@
-pub fn main() !void {}
+const std = @import("std");
+
+// "!void" means the function returns nothing and might throw an error
+pub fn main() !void {
+    std.debug.print("hi {s}", .{"mom"});
+}
